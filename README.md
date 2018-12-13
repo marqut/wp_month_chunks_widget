8bebc11aed9d792321dc263fbf234b9bb59477af
Yakumo Saki <yakumo@ziomatrix.org>
2018-12-13 16:18:38 +0900

    fixed: replace deprecated syntax

---
+++ README.md
@@ -0,0 +1,24 @@
+# wp_monthchunks_widget
+
+## What is this ?
+WordPressプラグインです。このプラグインは、
+月別アーカイブをもっとコンパクトに表示するウィジェットを提供します。
+wp_monthchunks というプラグインと同等の表示をします。
+
+Original idea from Justin.  
+http://justinsomnia.org/2005/04/monthchunks-howto/
+
+## 対応Wordpressバージョン
+最新版で動作確認しています（自前のblogで使用しているので）
+
+## 使い方
+プラグインをインストールすると、ウィジェットに MonthChunksWidgetが追加されます。
+あとは普通のウィジェットと同じように扱ってください。
+
+## 追記
+http://wordpress.org/plugins/monthchunks/
+どうも、公式のところに同じ機能を持つプラグインが登録されています。
+自動アップデート対応などを考えるとこちらを利用した方が良いかもしれません。
+
+## 更新履歴
+* 2018/12/13 WP_DEBUG = TRUE の際の警告がでないように修正
--- wp_monthchunks_widget.php
+++ wp_monthchunks_widget.php
@@ -1,31 +1,30 @@
 <?php
 /*
 Plugin Name: WP Monthchunks widget
-Plugin URI: http://www.ziomatrix.org/download/wp_monthchunks_widget
+Plugin URI: https://github.com/yakumo-saki/wp_month_chunks_widget
 Description: コンパクトな月表示ウィジェットを提供します
-Version: 0.02
-Author: marqut
-Author URI: http://www.ziomatrix.org/
+Version: 0.03
+Author: Yakumo Saki
+Author URI: https://github.com/yakumo-saki/wp_month_chunks_widget
 */
- 
 // thanks to http://weble.org/2011/04/19/wordpress-beginner-plugin
 
 class MonthChunksWidget extends WP_Widget {
     /** constructor */
-    function MonthChunksWidget() {
-        parent::WP_Widget(false, $name = 'MonthChunksWidget');	
+    function __construct() {
+        parent::__construct(false, $name = 'MonthChunksWidget');
     }
 
     /** @see WP_Widget::widget */
-    function widget($args, $instance) {		
+    function widget($args, $instance) {
         extract( $args );
         global $wpdb;
 
         $title = apply_filters('widget_title', $instance['title']);
-        
+
         // get months
         $result = $wpdb->get_results( "select distinct DATE_FORMAT(post_date, '%Y-%m') as ym FROM wp_posts WHERE post_status = 'publish' ORDER BY DATE_FORMAT(post_date, '%Y') desc,DATE_FORMAT(post_date, '%m') asc" );
-        
+
         // 以下、表示テンプレート
         ?>
               <?php echo $before_widget; ?>
@@ -37,33 +36,18 @@ class MonthChunksWidget extends WP_Widget {
                   }
 
                   // thisyear = ブレイクキー / next =>次のデータの年
-                  $thisyear = substr($result[0]->ym,0,4); 
+                  $thisyear = substr($result[0]->ym,0,4);
                   echo $thisyear . "<br />";
                   for ($i = 0; $i < count($result) - 1; $i++ ) {
-                      // 年が変わっていた場合は年を出力
-                      $nextyear = substr($result[$i]->ym,0,4);
-                      if ( $thisyear != $nextyear) {
-                          echo "</li>\n<li class='cat-item'>" . $nextyear . "<br />";
-                          $thisyear = $nextyear;
-                      }
-                      $thismonth = substr($result[$i]->ym,5,2);
-                      echo "<a href='" . get_month_link($thisyear, $thismonth) ."'>" . $thismonth . "</a>&nbsp;";
-                  }
-
-                  echo "</li></ul>";
-
-                  ?>
-              <?php echo $after_widget; ?>
-        <?php
-    }
+					  // 年が変わっていた場合は年を出力
 
     /** @see WP_Widget::update */
-    function update($new_instance, $old_instance) {				
+    function update($new_instance, $old_instance) {
         return $new_instance;
     }
 
     /** @see WP_Widget::form */
-    function form($instance) {				
+    function form($instance) {
         $title = esc_attr($instance['title']);
         ?>
             <p>
@@ -72,10 +56,9 @@ class MonthChunksWidget extends WP_Widget {
                 <input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo $title; ?>" />
                 </label>
             </p>
-        <?php 
+        <?php
     }
 
 } // class end
 
-add_action('widgets_init', create_function('', 'return register_widget("MonthChunksWidget");'));
-?>
+add_action('widgets_init', function() { register_widget("MonthChunksWidget");});
